2fda66deb4c1a3c5e1e9f472e9bc898a266954df
JaredP45 <synetos@protonmail.com>
2021-11-08 17:20:39 -0600

    Updated the formatting of the output for switch condition 2; Condition 3 still does not output if the input is correct.

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from BookRecord import SelectOption
+from BookRecord import SelectOption as So
 
 count = 0
 inputSelection = int(input('Enter your selection: '))
 while count < 5:
     # Initialize switch
-    switch = SelectOption('', '', '', '')
+    switch = So('', '', '', '')
 
     if inputSelection == 1:
         inputTitle = input('Enter Title: ')
         inputAuthor = input('Enter Author: ')
         inputDate = input('Enter Date Read: ')
         inputRating = input('Enter Rating: ')
-        switch = SelectOption(inputTitle, inputAuthor, inputDate, inputRating)
-        switch.option(1)
+        switch = So(inputTitle, inputAuthor, inputDate, inputRating)
+        switch.option(inputSelection)
+        count += 1
 
     elif inputSelection == 2:
-        # inputTitle = input('Enter Title: ')
-        # switch = SelectOption(inputTitle, '', '', '')
-        print(switch.option(2))
+        inputTitle = input('Enter Title: ')
+        switch = So(inputTitle)
+        switch.option(inputSelection)
 
     elif inputSelection == 3:
         inputAuthor = input('Enter Author Name: ')
-        switch = SelectOption('', inputAuthor, '', '')
-        print(switch.option(3))
+        switch = So(inputAuthor)
+        switch.option(inputSelection)
 
     inputSelection = int(input('Enter your selection: '))
-    count += 1
