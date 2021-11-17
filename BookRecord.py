4a8768c347d8f9853490e144f83f29560b5804ff
Jared <52184578+jaredp45@users.noreply.github.com>
2021-11-17 16:29:05 -0600

    jpaubel-general-update
    Made updates to file I/O; shifted segments of code where they are more appropriate.

--- BookRecord.py
+++ BookRecord.py
@@ -14,52 +14,50 @@ class SelectOption:
         self.rating = rating
 
     def case_1(self):
-        file = open('record.txt', 'a')
-        if validateDate(self.date):
-            book_listing = str(f'{self.title},{self.author},{self.date},{self.rating}\n')
-            file.write(book_listing)
-        file.close()
+        with open('record.txt', 'a') as file:
+            if validateDate(self.date):
+                book_listing = str(f'{self.title},{self.author},{self.date},{self.rating}\n')
+                file.write(book_listing)
 
     def case_2(self):
         search_title = self.title
-        found_title = True
-        file = open('record.txt')
-        lines = file.readlines()
+        found_title = False
+
+        with open('record.txt', 'r') as file:
+            lines = file.readlines()
+
         for line in lines:
             line = line.split(',')
             if search_title in line[0]:
                 print(f'{line[0]}, by {line[1]}, Read {line[2]}, Rating {line[3]}', end='')
                 found_title = True
-                break
-            else:
-                found_title = False
+
         if not found_title:
             print('Not Found.')
-        file.close()
 
     def case_3(self):
         search_author = self.author
-        found_author = True
-        file = open('record.txt')
-        lines = file.readlines()
+        found_author = False
+
+        with open('record.txt', 'r') as file:
+            lines = file.readlines()
+
         for line in lines:
             line = line.split(',')
             if search_author in line[1]:
                 print(f'{line[0]}, by {line[1]}, Read {line[2]}, Rating {line[3]}', end='')
                 found_author = True
-            else:
-                found_author = False
+
         if not found_author:
             print('Not Found.')
-        file.close()
 
     def case_4(self):
-        file = open('record.txt')
-        lines = file.readlines()
+        with open('record.txt', 'r') as file:
+            lines = file.readlines()
+
         for line in lines:
             line = line.split(',')
             print(f'{line[0]}, by {line[1]}, Read {line[2]}, Rating {line[3]}', end='')
-        file.close()
 
     def case_5(self):
         search_title = self.title
@@ -72,6 +70,4 @@ class SelectOption:
                 if search_title not in line.strip('\n'):
                     file.write(line)
 
-
-
-
+        print(f'Deleted {search_title}')
--- main.py
+++ main.py
@@ -3,20 +3,29 @@ from BookRecord import SelectOption as So
 if __name__ == '__main__':
     # Create switch instance
     switch = So()
-    end_program = False
+    inputSelection = 0
 
-    try:
-        inputSelection = int(input('Enter your selection: '))
-    except ValueError:
-        print('\nYou must enter a number.\n')
-        inputSelection = 0
     while inputSelection != 6:
+
+        print('\n 1. Enter a book\n',
+              '2. Search for a book\n',
+              '3. Search for an author\n',
+              '4. Display all books\n',
+              '5. Delete a book\n',
+              '6. Exit\n')
+
+        try:
+            inputSelection = int(input('\nEnter your selection: '))
+        except ValueError:
+            print('\nYou must enter a number.\n')
+            inputSelection = 0
 
         if inputSelection == 1:
             inputTitle = input('Enter Title: ')
             inputAuthor = input('Enter Author: ')
             inputDate = input('Enter Date Read (mm-dd-yyyy): ')
             inputRating = int(input('Enter Rating: '))
+
             while inputRating < 0 or inputRating > 10:
                 print('You must enter rating score in range of 0 to 10.')
                 inputRating = int(input('\nEnter Rating: '))
@@ -43,18 +52,5 @@ if __name__ == '__main__':
 
         elif inputSelection == 6:
             break
-
-        print('\n 1. Enter a book\n',
-              '2. Search for a book\n',
-              '3. Search for an author\n',
-              '4. Display all books\n',
-              '5. Delete a book\n',
-              '6. Exit')
-
-        try:
-            inputSelection = int(input('Enter your selection: '))
-        except ValueError:
-            print('\nYou must enter a number.\n')
-            inputSelection = 0
 
     print('Thanks for using this application!')
